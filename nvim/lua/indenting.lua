1ab89250b80d7870d0be2cae3fcee5383aacf442
Mikey Rivera <michael.rivera@shopee.com>
2023-07-18 10:18:07 +0800

    Change indenting

--- nvim/lua/indenting.lua
+++ nvim/lua/indenting.lua
@@ -1,14 +1,23 @@
 vim.opt.list = true
 vim.opt.listchars:append "space:⋅"
 vim.opt.termguicolors = true
+
 vim.cmd [[highlight IndentBlanklineIndent1 guibg=#000000 gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
 
+vim.cmd [[highlight Grey guifg=#363636 gui=nocombine]]
+
 require("indent_blankline").setup {
-    char = "",
+    show_current_context = true,
+    show_current_context_start = true,
+    --char = "",
+    --char_highlight_list = {
+        --"IndentBlanklineIndent1",
+        --"IndentBlanklineIndent2",
+    --},
+    space_char_blankline = " ",
     char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
+      "Grey"
     },
     space_char_highlight_list = {
         "IndentBlanklineIndent1",
